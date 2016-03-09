e29f532af595e62a8810ec6856ab7e1af87ce0dd
bonny <bonnylovecnblue@gmail.com>
2016-03-10 01:58:33 +0800

    comment

---
+++ 0225HW.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: big5 -*-
+print '系級:資管系107級'
+print '學號:A1033303'
+print '中文名字:張簡家琳'
+print '英文名字:CHANG CHIEN JIA-LIN'
+
+
+
---
+++ ch2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: big5 -*-
+myage=20
+_year=2016
+A=25
+a=150
+
+print myage
+print'今年是%d年' %_year
+print'大寫A等於%d,小寫a等於%d' %(A,a)
+_year=myage
+print'今年我%d歲' %_year
+
+B=20
+C=3
+D=50
+E=55
+print'B+C=',B+C
+print'C*D=',C*D
+print'B/D=',float(B/D)
+print'D%E=',D%E
+print'B>>C=',B>>C
+print'abs(-58)=',abs(-58)
